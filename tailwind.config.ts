aa20401c132f95c2a5f198d3c84ac3e0f7543a05
André Felipe Guimarães <65873168+andrejfg@users.noreply.github.com>
2024-01-31 10:29:17 -0300

    typescript tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+export default {
   content: ['./src/**/*.{tsx,jsx}', './app/**/*.{tsx,jsx}'],
   theme: {
     extend: {
@@ -9,4 +10,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+} satisfies Config
